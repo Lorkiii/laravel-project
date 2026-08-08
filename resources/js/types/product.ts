81e8d7688cb9631c8a73303af38972264f617520
Lorkiii <jrymnd18@gmail.com>
2026-08-08 21:39:23 +0800

    add brand and model in migration file and props

--- resources/js/types/product.ts
+++ resources/js/types/product.ts
@@ -12,17 +12,18 @@ export type Product = {
     category_id: number;
     category: string;
     brand: string;
+    model: string;
 };
 
 export type ProductFormValues = {
     name: string;
     sku: string;
     brand: string;
+    model: string;
     category: string;
     description: string;
-    price: string;
-    quantity: string;
-    minimum_stock: string;
+    price: number;
+    quantity: number;
+    minimum_stock: number;
     status: boolean;
 };
-
